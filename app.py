80188d93a1c057e1860ddbd7d61950a565eacb19
myathinnwe <59833500+myathinnwe@users.noreply.github.com>
2023-11-23 18:59:48 +1300

    Create app.py
    create simple python file

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import the Flask module
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route and its corresponding function
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Run the application if the script is executed
+if __name__ == '__main__':
+    app.run(debug=True)
